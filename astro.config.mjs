7492d247dd3dc3f36de688c98f4913323c791310
ubtvpn-eng <zolkinzzz@gmail.com>
2026-01-08 17:17:09 +0300

    Deployment fix: Save uncommited changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     // Замените на ваш адрес GitHub Pages, например: https://username.github.io
-    site: 'https://USERNAME.github.io',
+    site: 'https://ubtvpn-eng.github.io',
     // Замените на название вашего репозитория, например: /my-repo
-    base: '/REPO_NAME',
+    base: '/ubtvpn1',
 
     integrations: [tailwind()],
 });
